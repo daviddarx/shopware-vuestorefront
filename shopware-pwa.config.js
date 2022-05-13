a80c55556d00795937c4ebbb1e756571b5876ed7
David Darx <david.darx@liip.ch>
2022-05-13 12:11:17 +0200

    feat(setup) basic info

---
+++ shopware-pwa.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  shopwareEndpoint: "https://dev.cookieshop.dev.liip.ch",
+  shopwareAccessToken: "SWSCZXRWD210DTG0Q3C2QWTWVW",
+  shopwareApiClient: {
+    timeout: 5000,
+  },
+  shopwareDomainsAllowList: [
+    "http://localhost:3000",
+    "http://localhost:3000/en"
+  ]
+};
