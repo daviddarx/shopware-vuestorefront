837892615fd362320b0c57f1fcc731a766b6eb98
David Darx <david.darx@liip.ch>
2022-05-13 15:32:12 +0200

    setup(deploy) update config

--- shopware-pwa.config.js
+++ shopware-pwa.config.js
@@ -3,6 +3,10 @@ module.exports = {
   shopwareAccessToken: "SWSCZXRWD210DTG0Q3C2QWTWVW",
   shopwareApiClient: {
     timeout: 5000,
+    auth: {
+      username: "david.darx",
+      password: "david.darx"
+    }
   },
   shopwareDomainsAllowList: [
     "http://localhost:3000",
