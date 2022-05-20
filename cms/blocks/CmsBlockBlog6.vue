e6686bfdd852c98b26c5cce37ac228e7f9295a9e
David Darx <david.darx@liip.ch>
2022-05-20 11:28:11 +0200

    feat(blog) add components for blog block and elements

---
+++ cms/blocks/CmsBlockBlog6.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cms-block-blog">
+    <strong>BLOCK: NETZP BLOG 6</strong>
+
+    <!-- <pre>{{ getSlots }}</pre> -->
+
+    <CmsGenericElement
+      v-if="getSlots[0]"
+      :content="getSlots[0]"
+      class="cms-block-blog__element"
+    />
+  </div>
+</template>
+
+<script>
+import CmsGenericElement from "sw-cms/CmsGenericElement";
+
+export default {
+  name: "CmsBlockBlog6",
+  components: {
+    CmsGenericElement,
+  },
+  props: {
+    content: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    getSlots() {
+      return this.content.slots || [];
+    }
+  },
+  mounted () {
+    console.log("mounted", "block blog 6");
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .cms-block-blog {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+</style>
---
+++ cms/elements/CmsElementBlog6.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="cms-element-blog">
+    <strong>ELEMENT: NETZP BLOG 6</strong>
+
+    <h4>Kategorien:</h4>
+    <select>
+      <option
+        v-for="cat in content.data.result.aggregations.categories.entities"
+        :key="cat._uniqueIdentifier"
+      >
+        {{ cat.translated.title }}
+      </option>
+    </select>
+
+    <div>
+      <div
+        v-for="(el) in content.data.result.elements"
+        :key="el.id"
+        class="cms-element-blog__card"
+      >
+        <h2>{{el.translated.title}}</h2>
+        <p>{{el.postdate}}</p>
+        <p>{{el.category.translated.title}}</p>
+        <p v-html="el.translated.teaser"></p>
+        <nuxt-link
+          :to="'/news/'+el.translated.slug"
+        >
+          Read more...
+        </nuxt-link>
+        <!-- <pre>{{el}}</pre> -->
+      </div>
+    </div>
+
+    <pre>{{ content }}</pre>
+  </div>
+</template>
+
+<script>
+import CmsGenericElement from "sw-cms/CmsGenericElement";
+
+export default {
+  name: "CmsElementBlog6",
+  components: {
+    CmsGenericElement,
+  },
+  props: {
+    content: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+  },
+  mounted () {
+    console.log("mounted", "element blog 6");
+    console.log("all routes", this.$router.options.routes);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .cms-element-blog {
+    background-color: rgba(0, 0, 0, 0.1);
+
+    &__card {
+      border: 1px solid black;
+      padding: 1rem;
+
+      a {
+        background-color: green;
+        padding: 1em;
+      }
+    }
+  }
+</style>
