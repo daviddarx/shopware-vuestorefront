4d90f11354c1b556a08f7e847dd93a14c23abbc4
David Darx <david.darx@liip.ch>
2022-05-20 09:44:03 +0200

    feat(LeadWithImage) implement component

---
+++ cms/blocks/CmsBlockLeadWithImage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="cms-block-lead-with-image">
+    <strong>BLOCK: LEAD WITH IMAGE</strong>
+
+    <!-- <pre>{{ getSlots }}</pre> -->
+
+    <CmsGenericElement
+      v-if="getSlots[1]"
+      :content="getSlots[1]"
+      class="cms-block-lead-with-image__image"
+    />
+
+    <CmsGenericElement
+      v-if="getSlots[0]"
+      :content="getSlots[0]"
+      class="cms-block-lead-with-image__bottom"
+    />
+  </div>
+</template>
+
+<script>
+import CmsGenericElement from "sw-cms/CmsGenericElement";
+
+export default {
+  name: "CmsBlockLeadWithImage",
+  components: {
+    CmsGenericElement,
+  },
+  props: {
+    content: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    getSlots() {
+      return this.content.slots || [];
+    }
+  },
+  mounted () {
+    console.log("mounted", "block lead image");
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .cms-block-lead-with-image {
+    background-color: rgba(0, 0, 0, 0.1);
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+</style>
---
+++ cms/elements/CmsElementLead.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="cms-element-lead">
+    <strong>ELEMENT: LEAD</strong>
+
+    <!-- <pre>{{ content }}</pre> -->
+
+    <h1 v-text="content.translated.config.title.value"></h1>
+
+    <p v-html="content.translated.config.body.value"></p>
+  </div>
+</template>
+
+<script>
+import CmsGenericElement from "sw-cms/CmsGenericElement";
+
+export default {
+  name: "CmsElementLead",
+  components: {
+    CmsGenericElement,
+  },
+  props: {
+    content: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+  },
+  mounted () {
+    console.log("mounted", "element lead");
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .cms-element-lead {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+</style>
