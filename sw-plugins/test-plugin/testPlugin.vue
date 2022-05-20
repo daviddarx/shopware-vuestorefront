1d55206ba24c41964961061da11c7a1a88918c09
David Darx <david.darx@liip.ch>
2022-05-20 08:17:05 +0200

    feat(TestPlugin) create a local test plugin

---
+++ sw-plugins/test-plugin/testPlugin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test-plugin">
+    <h3>I am a test-plugin</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "TestPlugin",
+  data() {
+    return {};
+  },
+  mounted () {
+    console.log("test plugin mounted");
+  },
+};
+</script>
+<style lang="scss" scoped>
+.test-plugin {
+  display: flex;
+  justify-content: center;
+  border: 1px solid green;
+}
+</style>
